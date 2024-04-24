4ca01218a762823cabc378f0551c2d163086d614
David Maas <contact@pathogenstudios.com>
2024-04-24 14:14:35 -0500

    Created playground

---
+++ RxThrowingFinally/BonsaiExtensions.cs
@@ -0,0 +1,116 @@
+﻿/*
+Copyright (c) 2011-2024 Bonsai Foundation CIC and Contributors
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+of the Software, and to permit persons to whom the Software is furnished to do
+so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai;
+
+internal static class ObservableCombinators
+{
+    /// <summary>
+    /// Returns a connectable observable sequence that upon connection causes the <paramref name="source"/>
+    /// to push results into a new fresh subject, which is created by invoking the specified
+    /// <paramref name="subjectFactory"/>.
+    /// </summary>
+    /// <typeparam name="TSource">The type of the elements in the source sequence.</typeparam>
+    /// <typeparam name="TResult">The type of the elements in the result sequence.</typeparam>
+    /// <param name="source">The source sequence whose elements will be pushed into the specified subject.</param>
+    /// <param name="subjectFactory">
+    /// The factory function used to create the subject that notifications will be pushed into.
+    /// </param>
+    /// <returns>The reconnectable sequence.</returns>
+    public static IConnectableObservable<TResult> MulticastReconnectable<TSource, TResult>(this IObservable<TSource> source, Func<ISubject<TSource, TResult>> subjectFactory)
+    {
+        return new ReconnectableObservable<TSource, TResult>(source, subjectFactory);
+    }
+
+    /// <summary>
+    /// Returns a connectable observable sequence that upon connection causes the <paramref name="source"/>
+    /// to push results into a new fresh <see cref="Subject{TSource}"/>.
+    /// </summary>
+    /// <typeparam name="TSource">The type of the elements in the source sequence.</typeparam>
+    /// <param name="source">The source sequence whose elements will be pushed into the specified subject.</param>
+    /// <returns>The reconnectable sequence.</returns>
+    public static IConnectableObservable<TSource> PublishReconnectable<TSource>(this IObservable<TSource> source)
+    {
+        return MulticastReconnectable(source, () => new Subject<TSource>());
+    }
+}
+
+class ReconnectableObservable<TSource, TResult> : IConnectableObservable<TResult>
+{
+    readonly object gate;
+    readonly IObservable<TSource> observableSource;
+    readonly Func<ISubject<TSource, TResult>> subjectFactory;
+    IConnectableObservable<TResult> connectableSource;
+
+    public ReconnectableObservable(IObservable<TSource> source, Func<ISubject<TSource, TResult>> subjectSelector)
+    {
+        subjectFactory = subjectSelector;
+        observableSource = source.AsObservable();
+        gate = new object();
+    }
+
+    public IDisposable Connect()
+    {
+        lock (gate)
+        {
+            EnsureConnectableSource();
+            var connection = connectableSource.Connect();
+            return Disposable.Create(() =>
+            {
+                lock (gate)
+                {
+                    using (connection)
+                    {
+                        connectableSource = null;
+                    }
+                }
+            });
+        }
+    }
+
+    public IDisposable Subscribe(IObserver<TResult> observer)
+    {
+        lock (gate)
+        {
+            EnsureConnectableSource();
+            return connectableSource.Subscribe(observer);
+        }
+    }
+
+    private void EnsureConnectableSource()
+    {
+        if (connectableSource == null)
+        {
+            var subject = subjectFactory();
+            connectableSource = observableSource.Multicast(subject);
+        }
+    }
+}
---
+++ RxThrowingFinally/Program.cs
@@ -0,0 +1,118 @@
+﻿//==================================================================================================================
+// General
+//==================================================================================================================
+#define ASYNC_OBSERVABLE // Use an async observable
+
+//==================================================================================================================
+// Control how the observable ends (pick one or none)
+//==================================================================================================================
+#define COMPLETE_NORMALLY // End the observable with OnCompleted
+//#define ONERROR_OBSERVABLE // End the observable with an error explicitly
+//#define THROWING_OBSERVABLE // Throw an exception in the observable
+// (If none enabled: No explicit completion)
+
+//==================================================================================================================
+// Infinite sequence tests
+//==================================================================================================================
+//#define INFINITE_SEQUENCE // Make the observable sequence infinite (use one of the two to provide means of completion)
+//#define ASYNC_OBSERVABLE_CANCEL // Cancel the sequence before it completes
+//#define TAKE_3_ONLY // Take only 3 elements of the sequence (only makes sense with async observable)
+using Bonsai;
+using System.Reactive;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using System.Reflection;
+
+Console.WriteLine($"Hello, World! Using Rx.NET version {typeof(System.Reactive.IObserver<,>).Assembly.GetName().Version}");
+
+// Not applicable, it's the same between this and Bonsai (leaving here so I remember I checked it)
+Console.WriteLine($"Observable impl: {typeof(Observable).GetField("s_impl", BindingFlags.Static | BindingFlags.NonPublic).GetValue(null)}");
+
+void ObservableFunc(IObserver<int> observer, CancellationToken cancellationToken)
+{
+    Console.WriteLine("Observable task start...");
+
+#if INFINITE_SEQUENCE
+    for (int i = 0; !cancellationToken.IsCancellationRequested; i++)
+            observer.OnNext(i);
+#else
+    observer.OnNext(1);
+    observer.OnNext(2);
+    observer.OnNext(3);
+#endif
+
+#if THROWING_OBSERVABLE
+    Console.WriteLine("Observable is gonna go bang...");
+    throw new Exception("BANG");
+#elif ONERROR_OBSERVABLE
+    Console.WriteLine("Observable emitting OnError...");
+    observer.OnError(new Exception("ONERROR BANG"));
+#elif COMPLETE_NORMALLY
+    // Finally is still called if this is omitted, but the exception is never propagated !!!
+    Console.WriteLine("Observable explicitly completing.");
+    observer.OnCompleted();
+#else
+    Console.WriteLine("Observable ending without any explicit completion.");
+#endif
+}
+
+#if ASYNC_OBSERVABLE
+IObservable<int> source = Observable.Create<int>
+(
+    (observer, cancellationToken) =>
+    {
+        return Task.Factory.StartNew
+        (
+            () => ObservableFunc(observer, cancellationToken),
+            cancellationToken,
+            TaskCreationOptions.LongRunning,
+            TaskScheduler.Default
+        );
+    }
+);
+#else
+IObservable<int> source = Observable.Create<int>((observer) =>
+{
+    ObservableFunc(observer, CancellationToken.None);
+    return Disposable.Empty;
+});
+#endif
+
+// These do not seem to affect the outcome
+//source = source.PublishReconnectable().RefCount();
+
+#if TAKE_3_ONLY
+source = source.Take(3);
+#endif
+
+source = source.Finally(() =>
+{
+    Console.WriteLine("Finally gonna go bang...");
+    throw new Exception("FINALLY WENT BANG"); // We should expect the debugger to complain about this
+});
+
+CancellationTokenSource cts = new();
+
+Console.WriteLine("Subscribing...");
+source.Subscribe
+(
+    unit => Console.WriteLine($"Next: '{unit}'"),
+    ex => Console.Error.WriteLine($"Error: {ex}"),
+    () => Console.WriteLine("Completed"),
+    cts.Token
+);
+#if ASYNC_OBSERVABLE
+Console.WriteLine("Subscribled");
+Thread.Sleep(1000);
+#if ASYNC_OBSERVABLE_CANCEL
+Console.WriteLine("Canceling...");
+cts.Cancel();
+Console.WriteLine("Canceled");
+Thread.Sleep(500);
+#endif
+#endif
+
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("Goodbye (Should not reach here due to exception during finally action)");
+Console.ForegroundColor = ConsoleColor.Gray;
